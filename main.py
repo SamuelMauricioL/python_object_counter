9afb6945f43207bd7a595f09914d8ae503b17bfe
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-08-06 10:04:55 -0500

    chore: add resize image

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import cv2
 
-imagePath = "assets/black_dot.jpg"
+# from PIL import Image
 
-gray = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
+IMAGE_PATH = "assets/colors_dot.jpg"
+BASE_WIDTH = 200
 
-th, threshed = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+image_gray = cv2.imread(IMAGE_PATH, cv2.IMREAD_GRAYSCALE)
+
+### Resize Image
+"""
+Es necesario hacer un resize de la image deacuerdo al BASE_WITDH
+para no afectar los limites de los stride's(s1 && s2) definidos 
+"""
+height, width = image_gray.shape[:2]
+BASE_HEIGHT = int(BASE_WIDTH * height / width)
+resized_image = cv2.resize(image_gray, (BASE_WIDTH, BASE_HEIGHT))
+
+
+th, threshed = cv2.threshold(
+    resized_image,
+    100,
+    255,
+    cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU,
+)
 
 cnts = cv2.findContours(threshed, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
-s1 = 30
-s2 = 60
+s1 = 3
+s2 = 20
 xcnts = []
 for cnt in cnts:
     if s1 < cv2.contourArea(cnt) < s2:
         xcnts.append(cnt)
 
-cv2.imshow("img", threshed)
-cv2.waitKey(0)
+# cv2.imshow("img", threshed)
+# cv2.waitKey(0)
 
 print("\nDots number: {}".format(len(xcnts)))
